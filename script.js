512ecdfa328ed08dd8679d89b271586a33a4e4c2
komala1219 <123307320+komala1219@users.noreply.github.com>
2023-04-11 07:39:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function fibonacci(num) {
 // your code here
+
+	 if (num < 2) {
+    return num;
+  } else {
+    return fibonacci(num - 1) + fibonacci(num - 2);
+  }
 }
 
 module.exports = fibonacci;
